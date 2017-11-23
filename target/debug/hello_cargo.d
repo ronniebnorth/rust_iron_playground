cb3c0bef733a6446965c68f39b2f701f72cfcd1c
Ronnie North <ronnienorth@gmail.com>
2017-11-23 17:01:40 -0500

    Remove the now ignored directory "target"

--- target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/home/rono/rust_projects/hello_cargo/target/debug/hello_cargo: /home/rono/rust_projects/hello_cargo/src/static/index.html /home/rono/rust_projects/hello_cargo/src/main.rs
--- target/debug/libhello_cargo.d
+++
@@ -1,1 +0,0 @@
-/home/rono/rust_projects/hello_cargo/target/debug/libhello_cargo.rmeta: /home/rono/rust_projects/hello_cargo/src/main.rs
